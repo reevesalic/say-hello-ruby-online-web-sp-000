88d911791ece894ec8fdad4a6367291e5bd4376b
Alicia Reeves <reevesalic@gmail.com>
2020-06-05 23:33:55 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,5 @@
 # Buisayld your say_hello method here
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts ("Hello #{name}!")
 
@@ -6,3 +7,12 @@ end
 
 #say_hello (name)
 #say_hello ("Ruby Programmer")
+=======
+say_hello (name = "Sophie", language = "Ruby Programmer")
+  puts ("Hello #{name}!")
+  puts "Hello #{language}!"
+end
+
+say_hello (name)
+say_hello (language)
+>>>>>>> 11362fe1fa52bf78b2b5dcca68314e161c1a2227
