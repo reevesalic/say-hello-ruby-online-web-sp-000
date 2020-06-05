088498c868ca7ce2f91d269fde55885cf793c08d
Alicia Reeves <reevesalic@gmail.com>
2020-06-05 23:16:20 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
-# Build your say_hello method here
+# Buisayld your say_hello method here
+say_hello (name)
+  puts ("Hello #{name}!")
+  puts "Hello #{Ruby Programmer}!"
+end
+
+say_hello (name)
+say_hello ("Ruby Programmer")
